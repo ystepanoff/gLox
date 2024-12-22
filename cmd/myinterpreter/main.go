0ff37cedc85bd5039fa5b0905e26619d962d480e
Yegor Stepanov <e.stepanov@draftkings.com>
2024-12-22 14:18:16 +0000

    Some small refactoring

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -9,12 +9,11 @@ func main() {
 	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
 
 	if len(os.Args) < 3 {
-		fmt.Fprintln(os.Stderr, "Usage: ./your_program.sh tokenize <filename>")
+		fmt.Fprintln(os.Stderr, "Usage: ./myinterpreter tokenize <filename>")
 		os.Exit(1)
 	}
 
 	command := os.Args[1]
-
 	if command != "tokenize" {
 		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", command)
 		os.Exit(1)
@@ -23,12 +22,12 @@ func main() {
 	filename := os.Args[2]
 	fileContents, err := os.ReadFile(filename)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error reading file '%s': %v\n", filename, err)
 		os.Exit(1)
 	}
-
 	if len(fileContents) > 0 {
-		panic("Scanner not implemented")
+		fmt.Fprintln(os.Stderr, "Scanner not implemented")
+		os.Exit(1)
 	} else {
 		fmt.Println("EOF  null") // Placeholder, remove this line when implementing the scanner
 	}
