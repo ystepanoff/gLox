6adf07683c89cf3dee9ba5fcbcbec648e5a99a59
Yegor Stepanov <e.stepanov@draftkings.com>
2024-12-27 05:48:07 +0000

    Scanning: Other single-character tokens

--- pkg/scanner/scanner.go
+++ pkg/scanner/scanner.go
@@ -45,6 +45,18 @@ func (s *Scanner) scanToken() {
 		s.addToken(LEFT_BRACE)
 	case '}':
 		s.addToken(RIGHT_BRACE)
+	case ',':
+		s.addToken(COMMA)
+	case '.':
+		s.addToken(DOT)
+	case '-':
+		s.addToken(MINUS)
+	case '+':
+		s.addToken(PLUS)
+	case ';':
+		s.addToken(SEMICOLON)
+	case '*':
+		s.addToken(STAR)
 	}
 }
 
