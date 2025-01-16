0a3a91e55ab780c2864bee6237252bdb3d7908aa
Yegor Stepanov <ystepanoff@icloud.com>
2025-01-16 02:49:57 +0000

    Evaluating expressions: Booleans & nil

---
+++ pkg/interpreter/interpreter.go
@@ -0,0 +1,34 @@
+package interpreter
+
+import (
+	"fmt"
+
+	"github.com/codecrafters-io/interpreter-starter-go/pkg/parser"
+)
+
+type Interpreter struct{}
+
+func NewInterpreter() *Interpreter {
+	return &Interpreter{}
+}
+
+func (i *Interpreter) VisitBinary(binary *parser.Binary) interface{} {
+	return nil
+}
+
+func (i *Interpreter) VisitGrouping(grouping *parser.Grouping) interface{} {
+	return nil
+}
+
+func (i *Interpreter) VisitLiteral(literal *parser.Literal) interface{} {
+	return literal.Value
+}
+
+func (i *Interpreter) VisitUnary(unary *parser.Unary) interface{} {
+	return nil
+}
+
+func (i *Interpreter) Interpret(expression parser.Expression) {
+	value := expression.Accept(i)
+	fmt.Println(value)
+}
